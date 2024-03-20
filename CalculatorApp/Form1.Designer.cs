67f48e367b9b502050793ab716f0fcb4a124c47a
Soichiro Inatani <inai17ibar@live.jp>
2024-03-20 11:10:25 +0900

    ウィンドウズアプリケーションのUIを作成した。

--- CalculatorApp/Form1.Designer.cs
+++ CalculatorApp/Form1.Designer.cs
@@ -28,12 +28,333 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            button21 = new Button();
+            button22 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Yu Gothic UI", 12F);
+            textBox1.Location = new Point(18, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(424, 34);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 12F);
+            button1.Location = new Point(18, 65);
+            button1.Name = "button1";
+            button1.Size = new Size(63, 46);
+            button1.TabIndex = 1;
+            button1.Text = "M+";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Yu Gothic UI", 12F);
+            button2.Location = new Point(103, 65);
+            button2.Name = "button2";
+            button2.Size = new Size(63, 46);
+            button2.TabIndex = 2;
+            button2.Text = "M-";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Yu Gothic UI", 12F);
+            button3.Location = new Point(191, 65);
+            button3.Name = "button3";
+            button3.Size = new Size(63, 46);
+            button3.TabIndex = 3;
+            button3.Text = "MRC";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Yu Gothic UI", 12F);
+            button4.Location = new Point(191, 127);
+            button4.Name = "button4";
+            button4.Size = new Size(63, 46);
+            button4.TabIndex = 6;
+            button4.Text = "9";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Yu Gothic UI", 12F);
+            button5.Location = new Point(103, 127);
+            button5.Name = "button5";
+            button5.Size = new Size(63, 46);
+            button5.TabIndex = 5;
+            button5.Text = "8";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Yu Gothic UI", 12F);
+            button6.Location = new Point(18, 127);
+            button6.Name = "button6";
+            button6.Size = new Size(63, 46);
+            button6.TabIndex = 4;
+            button6.Text = "7";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Yu Gothic UI", 12F);
+            button7.Location = new Point(191, 188);
+            button7.Name = "button7";
+            button7.Size = new Size(63, 46);
+            button7.TabIndex = 9;
+            button7.Text = "6";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Yu Gothic UI", 12F);
+            button8.Location = new Point(103, 188);
+            button8.Name = "button8";
+            button8.Size = new Size(63, 46);
+            button8.TabIndex = 8;
+            button8.Text = "5";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Yu Gothic UI", 12F);
+            button9.Location = new Point(18, 188);
+            button9.Name = "button9";
+            button9.Size = new Size(63, 46);
+            button9.TabIndex = 7;
+            button9.Text = "4";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Yu Gothic UI", 12F);
+            button10.Location = new Point(191, 249);
+            button10.Name = "button10";
+            button10.Size = new Size(63, 46);
+            button10.TabIndex = 12;
+            button10.Text = "3";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Yu Gothic UI", 12F);
+            button11.Location = new Point(103, 249);
+            button11.Name = "button11";
+            button11.Size = new Size(63, 46);
+            button11.TabIndex = 11;
+            button11.Text = "2";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Yu Gothic UI", 12F);
+            button12.Location = new Point(18, 249);
+            button12.Name = "button12";
+            button12.Size = new Size(63, 46);
+            button12.TabIndex = 10;
+            button12.Text = "1";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Yu Gothic UI", 12F);
+            button13.Location = new Point(191, 311);
+            button13.Name = "button13";
+            button13.Size = new Size(63, 46);
+            button13.TabIndex = 15;
+            button13.Text = ".";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Font = new Font("Yu Gothic UI", 12F);
+            button14.Location = new Point(103, 311);
+            button14.Name = "button14";
+            button14.Size = new Size(63, 46);
+            button14.TabIndex = 14;
+            button14.Text = "00";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.Font = new Font("Yu Gothic UI", 12F);
+            button15.Location = new Point(18, 311);
+            button15.Name = "button15";
+            button15.Size = new Size(63, 46);
+            button15.TabIndex = 13;
+            button15.Text = "0";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.Font = new Font("Yu Gothic UI", 12F);
+            button16.Location = new Point(277, 188);
+            button16.Name = "button16";
+            button16.Size = new Size(63, 46);
+            button16.TabIndex = 17;
+            button16.Text = "×";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Font = new Font("Yu Gothic UI", 12F);
+            button17.Location = new Point(277, 127);
+            button17.Name = "button17";
+            button17.Size = new Size(63, 46);
+            button17.TabIndex = 16;
+            button17.Text = "C";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Font = new Font("Yu Gothic UI", 12F);
+            button18.Location = new Point(277, 249);
+            button18.Name = "button18";
+            button18.Size = new Size(63, 108);
+            button18.TabIndex = 18;
+            button18.Text = "+";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.Font = new Font("Yu Gothic UI", 12F);
+            button19.Location = new Point(379, 311);
+            button19.Name = "button19";
+            button19.Size = new Size(63, 46);
+            button19.TabIndex = 22;
+            button19.Text = "=";
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.Font = new Font("Yu Gothic UI", 12F);
+            button20.Location = new Point(379, 249);
+            button20.Name = "button20";
+            button20.Size = new Size(63, 46);
+            button20.TabIndex = 21;
+            button20.Text = "-";
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += button20_Click;
+            // 
+            // button21
+            // 
+            button21.Font = new Font("Yu Gothic UI", 12F);
+            button21.Location = new Point(379, 188);
+            button21.Name = "button21";
+            button21.Size = new Size(63, 46);
+            button21.TabIndex = 20;
+            button21.Text = "÷";
+            button21.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            button22.Font = new Font("Yu Gothic UI", 12F);
+            button22.Location = new Point(379, 127);
+            button22.Name = "button22";
+            button22.Size = new Size(63, 46);
+            button22.TabIndex = 19;
+            button22.Text = "AC";
+            button22.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(460, 376);
+            Controls.Add(button19);
+            Controls.Add(button20);
+            Controls.Add(button21);
+            Controls.Add(button22);
+            Controls.Add(button18);
+            Controls.Add(button16);
+            Controls.Add(button17);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+        private Button button21;
+        private Button button22;
     }
 }
--- CalculatorApp/Form1.cs
+++ CalculatorApp/Form1.cs
@@ -6,5 +6,85 @@ namespace CalculatorApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
